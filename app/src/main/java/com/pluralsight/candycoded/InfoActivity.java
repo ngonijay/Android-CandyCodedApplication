1b89ae023b5593e2e49e18e799d8956bcc359729
Ngonidzashe.Jambo <R0pafadz0@80>
2022-08-29 15:27:02 +0200

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -36,10 +36,14 @@ public class InfoActivity extends AppCompatActivity {
     // ***
 
     public void createMapIntent(View view){
-        Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+        Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
+
         mapIntent.setPackage("com.google.android.apps.maps");
+
         if (mapIntent.resolveActivity(getPackageManager()) != null)
+
         startActivity(mapIntent);
     }
 
